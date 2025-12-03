c779fd87d15b3349e93a522042e97984b12ec064
Boy Maas <bitgnosys@bitgnosys-mac-2.local>
2025-12-03 15:58:15 +0100

    fix: add var declaration to register_plugin for strict mode compatibility

--- js/quad-net.js
+++ js/quad-net.js
@@ -1,7 +1,7 @@
 function on_init() {
 }
 
-register_plugin = function (importObject) {
+var register_plugin = function (importObject) {
     importObject.env.ws_connect = ws_connect;
     importObject.env.ws_is_connected = ws_is_connected;
     importObject.env.ws_send = ws_send;
